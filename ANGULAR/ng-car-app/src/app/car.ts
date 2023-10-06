e23260f9d07e1c960948650520d8714960b0362d
Achille Maboa <achille.maboa@icloud.com>
2023-10-06 11:24:31 +0200

    creation fichier car.ts

---
+++ ANGULAR/ng-car-app/src/app/car.ts
@@ -0,0 +1,11 @@
+export class car{
+    id: number;
+    name: string;
+    year: number;
+    type: string;
+    km: number;
+    price: number;
+    picture: string;
+    category: string;
+    created: Date;
+}
